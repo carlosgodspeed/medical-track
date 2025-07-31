298fbd39ae29bd16758491b461c6f14bc5a55080
carlosgodspeed <carlosminibics@gmail.com>
2025-07-31 18:18:37 -0300

    tentando corrigir um erro no displayname

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
-import { Image, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useEffect, useState } from 'react';
+import { Image, Text, View } from 'react-native';
 import { getLocalStorage } from '../service/Storage';
 export default function Header() {
 
+    const [user,setUser] = useState();
     useEffect(() => {
         GetUserDetail();
     },[])
@@ -10,17 +12,33 @@ export default function Header() {
     const GetUserDetail = async () => {
         const userInfo = await getLocalStorage('userDetail');
         console.log(userInfo);
+        setUser(userInfo);
     }
 
     return (
+        <View style={{
+            marginTop:20
+        }}>
         <View>
+            <View style={{
+                display:'flex',
+                flexDirection:'row',
+                alignItems:'center',
+                gap:10,
+            }}>
             <Image source={require('../assets/images/smiley.png')} 
             style={{
                 width:45,
                 height:45,
-
             }}
-            />
+        />
+        <Text style={{
+            fontSize:30,
+            fontWeight:'bold',
+        }}>{user.displayName}</Text>
         </View>
+        <Ionicons name="settings-outline" size={24} color="black"/>
+    </View>
+</View>
     )
 }
